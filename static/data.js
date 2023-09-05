f5be28297f2b17c12083661fa24192b4712f92cc
pocom311121314 <pocom311121314@gmail.com>
2023-09-05 20:12:56 +0530

    wd

--- static/data.js
+++ static/data.js
@@ -645,7 +645,7 @@ function filterProduct(value) {
 }
 
 //Search button click
-document.getElementById("search").addEventListener("click", () => {
+document.getElementById("search-input").addEventListener("click", () => {
   //initializations
   let searchInput = document.getElementById("search-input").value;
   let elements = document.querySelectorAll(".product-tag");
